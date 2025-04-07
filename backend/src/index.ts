5f581ccde3d3d7aa8b2eb833c6faba9a05bbf1d4
rajmanna <raj2021manna@gmail.com>
2025-04-07 21:06:19 +0530

    fixed /blog/blogs route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,10 @@
 import { Hono } from 'hono';
 import rootRoute from './routes/index.route';
+import { cors } from 'hono/cors';
 
 const app = new Hono();
+
+app.use(cors());
 
 app.route('/api/v1', rootRoute);
 
--- backend/src/routes/blog.route.ts
+++ backend/src/routes/blog.route.ts
@@ -12,12 +12,10 @@ import authMiddleware from '../middlewares/auth.middleware';
 const router = new Hono();
 
 router.get('/bulk', getAllBlogs);
-router.get('/:id', getBlogById);
-
-// Protected routes
+router.get('/blogs', authMiddleware, getBlogsByUser);
+router.get('/:id', authMiddleware, getBlogById);
 router.post('/', authMiddleware, createBlog);
 router.put('/:id', authMiddleware, updateBlogById);
-router.get('/blogs', authMiddleware, getBlogsByUser);
 router.delete('/:id', authMiddleware, deleteBlogById);
 
 export default router;
