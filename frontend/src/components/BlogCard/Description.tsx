e20d987c9e50619d0796e081ed790f919c2b0bf9
rajmanna <dev.rajm8923@gmail.com>
2025-04-14 22:44:26 +0530

    updated description component

--- frontend/src/components/BlogCard/Description.tsx
+++ frontend/src/components/BlogCard/Description.tsx
@@ -1,7 +1,11 @@
+import Markdown from 'react-markdown';
+
 function Description({ content }: { content: string }) {
   return (
     <div className="text-slate-600 leading-tight">
-      {content.length > 124 ? content.slice(0, 125) + '...' : content}
+      <Markdown>
+        {content.length > 124 ? content.slice(0, 125) + '...' : content}
+      </Markdown>
     </div>
   );
 }
